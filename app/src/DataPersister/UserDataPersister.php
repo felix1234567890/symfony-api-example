f7d9ac0d6c8a72b33665278cd6ef5e5466e11ce6
felix1234567890 <blueskyndsea01@gmail.com>
2020-10-30 21:23:42 +0100

    Solved task

---
+++ app/src/DataPersister/UserDataPersister.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\DataPersister;
+
+
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserDataPersister implements ContextAwareDataPersisterInterface
+{
+    private $em;
+    private $encoder;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordEncoderInterface $encoder)
+    {
+        $this->em = $em;
+        $this->encoder = $encoder;
+    }
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof User;
+    }
+
+    public function persist($data, array $context = [])
+    {
+        if ($data->getPassword()) {
+            $data->setPassword(
+                $this->encoder->encodePassword(
+                    $data,
+                    $data->getPassword()
+                )
+            );
+
+            $data->eraseCredentials();
+        }
+
+        $this->em->persist($data);
+        $this->em->flush();
+    }
+
+    public function remove($data, array $context = [])
+    {
+        $this->em->remove($data);
+        $this->em->flush();
+    }
+}
---
+++ app/src/Entity/Article.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ * @ORM\HasLifecycleCallbacks()
+ * @ApiResource(normalizationContext={"groups"={"article:read"}, "swagger_definition_name"="read"},denormalizationContext={"groups"={"article:write"}, "swagger_definition_name"="write"},
+ * itemOperations={"get"={"normalization_context"={"groups"={"user:read","article:read"}}},"put","delete"}
+)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @Groups({"article:read"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"article:read","article:write","user:item:get"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min="3")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Groups({"article:read","article:write", "user:item:get"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min="10")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups("article:read")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups("article:read")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"article:read","article:write"})
+     */
+    private $author;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+    /**
+     * @ORM\PreUpdate
+     */
+    public function setUpdatedAtValue() {
+        $this->setUpdatedAt(new \DateTime());
+    }
+}
